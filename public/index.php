269687e79642632a59f34244dc5b0b3d18b3d8de
fterenzani <f.terenzani@gmail.com>
2015-11-22 23:45:23 +0100

    Bug fix

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require __DIR__.'/../bootstrap.php';
 $routing = require __DIR__.'/../routing.php';
 
-$r = $container['router'];
+$r = $di['router'];
 $r->urls = $routing;
 
 if (Socrate\Router::NOT_FOUND === $r->dispatch(ltrim(@$_GET['_url'], '/'))) {
